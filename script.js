08fcee3d2f4686925d3b1658a61c8c91eda3d3f0
pikachu <kerieneriada@gmail.com>
2024-08-04 22:59:35 +0300

    save changes 04/08

--- script.js
+++ script.js
@@ -43,3 +43,17 @@ document.addEventListener('DOMContentLoaded', function() {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        // Remove active class from all links
+        navLinks.forEach(link => link.classList.remove('active'));
+        // Add active class to the clicked link
+        this.classList.add('active');
+      });
+    });
+  });
+  
